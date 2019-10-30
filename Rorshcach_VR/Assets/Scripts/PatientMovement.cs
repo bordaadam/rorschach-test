c928050f89f13791f6262b8d5a360eb13aa5b866
Borda Ádám <borda.adam3@gmail.com>
2019-10-30 17:46:47 +0100

    Very simple movement is added

---
+++ Rorshcach_VR/Assets/Scripts/PatientMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Library.Utilities;
+
+public class PatientMovement : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed = 5f;
+
+    private Rigidbody rigidbody;
+    private Vector3 movement;
+
+    void Awake() 
+    {
+        rigidbody = gameObject.GetRequiredComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        movement = movement.normalized * movementSpeed;
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+
+    void FixedUpdate() 
+    {
+        rigidbody.velocity = movement;
+    }
+
+}
---
+++ Rorshcach_VR/Assets/Scripts/Utils.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Library.Utilities
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Sets properties of the calling transform to a given transform.
+        /// </summary>
+        /// <param name="transform">
+        /// The transform that should be copied
+        /// </param>
+        /// <example>
+        /// public Transform myTransform;
+        /// (this.)transform.SetProperties(myTransform);
+        /// </example>
+        /// <remarks>
+        /// This is a method extension, so the first parameter should be "ignored".
+        /// </remarks>
+        public static void SetProperties(this Transform current, Transform transform)
+        {
+            current.position = transform.position;
+            current.rotation = transform.rotation;
+            current.localScale = transform.localScale;
+        }
+
+        /// <summary>
+        /// Gets specific component that is required for this gameobject.
+        /// </summary>
+        /// <example>
+        /// public Light li;
+        /// li = gameObject.GetRequiredComponent&lt;Light&gt;();
+        /// </example>
+        /// <remarks>
+        /// It logs if there's a missing component, so checking if a component is null is not required.
+        /// </remarks>
+        public static T GetRequiredComponent<T>(this GameObject obj) where T : Component
+        {
+            T component = obj.GetComponent<T>();
+
+            if(component == null)
+                Debug.LogWarning("Expected to find component of type " + typeof(T) + " on '" + obj.name + "' but found none.");
+
+            return component;
+        }
+
+    }
+}
