67ed9fef07c623e1a32fc617a3e3bfd90e0c2232
Borda Ádám <borda.adam3@gmail.com>
2019-11-11 13:37:18 +0100

    step sound is added

--- Rorshcach_VR/Assets/Scripts/PatientMovement.cs
+++ Rorshcach_VR/Assets/Scripts/PatientMovement.cs
@@ -7,6 +7,7 @@ public class PatientMovement : MonoBehaviour
 {
 
     [SerializeField] private float movementSpeed = 5f;
+    private AudioSource audioSource;
 
     private Rigidbody rigidbody;
     private Vector3 movement;
@@ -14,6 +15,7 @@ public class PatientMovement : MonoBehaviour
     void Awake() 
     {
         rigidbody = gameObject.GetRequiredComponent<Rigidbody>();
+        audioSource = gameObject.GetRequiredComponent<AudioSource>();
     }
 
     void Update()
@@ -21,6 +23,10 @@ public class PatientMovement : MonoBehaviour
         movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         movement = movement.normalized * movementSpeed;
         transform.eulerAngles = new Vector3(0, 0, 0);
+        if(movement != Vector3.zero && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 
     void FixedUpdate() 
