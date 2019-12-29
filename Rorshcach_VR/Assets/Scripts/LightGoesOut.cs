06f75c239977ed6c1609014cfb458d3a817d4c27
Borda Ádám <borda.adam3@gmail.com>
2019-12-29 17:46:48 +0100

    Lamp sound added + particle system

--- Rorshcach_VR/Assets/Scripts/LightGoesOut.cs
+++ Rorshcach_VR/Assets/Scripts/LightGoesOut.cs
@@ -7,21 +7,25 @@ public class LightGoesOut : MonoBehaviour
 
     [SerializeField] private Light light;
     [SerializeField] private float min, max;
+    [SerializeField] private AudioSource source;
+    [SerializeField] private ParticleSystem particleSystem;
 
     private void OnTriggerEnter(Collider other) 
     {
         if(other.GetComponent<PatientMovement>())
         {
-            StartCoroutine(Wait(Random.Range(min, max)));
+            source.Play();
+            particleSystem.Play();
+            StartCoroutine(Wait());
         }
     }
 
 
-    IEnumerator Wait(float t)
+    IEnumerator Wait()
     {
         while(true)
         {
-            yield return new WaitForSeconds(t);
+            yield return new WaitForSeconds(Random.Range(min, max));
             light.enabled = !light.enabled;
         }
     }
